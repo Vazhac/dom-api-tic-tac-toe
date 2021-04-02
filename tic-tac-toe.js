7bc9be971edc07971517838e417c7dbed2a8d257
Andru <dr3wwatkins@gmail.com>
2021-04-02 15:21:33 -0500

    created winning array

---
+++ tic-tac-toe.js
@@ -0,0 +1,13 @@
+//3 squares in a row, vertical, horizontal, diagonal.
+//if the click count is odd x
+//if the click coutn is even o
+
+//get id of the square of the id we click on
+  //hold the value
+
+  const winningConditions = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], [2, 5, 8],
+      [0, 3, 6], [0, 4, 8], [2, 4, 6], [1, 4, 7]
+  ]
+
+  
