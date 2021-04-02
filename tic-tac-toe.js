0bcfffc97d8fbd890f3f232a085435a2925dfd20
Andru <dr3wwatkins@gmail.com>
2021-04-02 16:04:19 -0500

    Added click event functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,19 +5,31 @@
 //get id of the square of the id we click on
 //hold the value
 
-const winningConditions = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], [2, 5, 8],
-    [0, 3, 6], [0, 4, 8], [2, 4, 6], [1, 4, 7]
-]
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('tic-tac-toe')
 
+    document.addEventListener("click", (event1) => {
+    const targetID = event1.target.id;
+    let currentPlayer = 'X';
+    let gameState = ["", "", "", "", "", "", "", "", "",]
+    // const newGame
 
-function RestartGame() {
-    currentPlayer = "X";
-    gameState = []
+    const winningConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], [2, 5, 8],
+        [0, 3, 6], [0, 4, 8], [2, 4, 6], [1, 4, 7]
+    ]
 
-}
-//eventListener listens for click on a square
-//add a counter for each successful click.
+        if ()
+
+    function restartGame() {
+        currentPlayer = "X";
+        gameState = [];
+
+    }
+    //eventListener listens for click on a square
+    //add a counter for each successful click.
     //if odd num of clicks; add inputs to player x array;
     //if even num of clicks; add inputs to player y array;
     //if no winners are chosen under 10 clicks, then the result is a draw;
+    })
+})
