05070e3efd6928b7cf53f154ef0c2bf8e966e1b9
Vazha Chiaberashvili <chiaberashv1@gmail.com>
2021-04-02 16:45:29 -0400

    Click Condition

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,11 +3,21 @@
 //if the click coutn is even o
 
 //get id of the square of the id we click on
-  //hold the value
+//hold the value
 
-  const winningConditions = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8], [2, 5, 8],
-      [0, 3, 6], [0, 4, 8], [2, 4, 6], [1, 4, 7]
-  ]
+const winningConditions = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [2, 5, 8],
+    [0, 3, 6], [0, 4, 8], [2, 4, 6], [1, 4, 7]
+]
 
-  
+
+function RestartGame() {
+    currentPlayer = "X";
+    gameState = []
+
+}
+//eventListener listens for click on a square
+//add a counter for each successful click.
+    //if odd num of clicks; add inputs to player x array;
+    //if even num of clicks; add inputs to player y array;
+    //if no winners are chosen under 10 clicks, then the result is a draw;
